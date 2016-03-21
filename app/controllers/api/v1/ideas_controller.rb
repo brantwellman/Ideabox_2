6240112770de530fdac36ac26d59661e9658534d
Brant Wellman <BrantWellman@gmail.com>
2016-03-21 15:41:46 -0600

    Add create and index api endpoints
    Add api controller
    Index, create functional and tested
    Methods for destroy and edit
    Add Responders gem

---
+++ app/controllers/api/v1/ideas_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::IdeasController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Idea.all
+  end
+
+  def create
+    idea = Idea.create(idea_params)
+    respond_with :api, :v1, idea
+  end
+
+  def destroy
+    idea = Idea.find(params[:id])
+    respond_with idea.delete
+  end
+
+  def update
+    idea = Idea.find(params[:id])
+    respond_with idea.update(idea_params)
+  end
+
+  private
+
+    def idea_params
+      params.require(:idea).permit(:title, :body, :quality)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root 'root#home'
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :ideas, only: [:index, :create, :show]
+    end
+  end
 end
